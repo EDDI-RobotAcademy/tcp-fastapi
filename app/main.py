5750b6d31132db868b0233dc46f71e1d3425870f
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-22 16:37:06 +0900

    feat: [한재혁] Exponential regression 구현 [TCPF]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from exponential_regression.controller.exponential_regression_controller import exponentialRegressionRouter
+
 app = FastAPI()
 
 
@@ -13,3 +15,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str]=None):
     return {"item_id": item_id, "q": q}
+
+
+app.include_router(exponentialRegressionRouter)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, port=33333)
