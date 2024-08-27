881e209cea864b48b11089a8e995a445865b283b
y0ng98 <dldydgnl2080@gmail.com>
2024-08-27 14:10:33 +0900

    fix: [이용휘] openAI api 추론에 대한 응답을 수신 [TCPF]

--- openai_api_test/service/openai_api_test_service_impl.py
+++ openai_api_test/service/openai_api_test_service_impl.py
@@ -1,10 +1,23 @@
+import os
+import sys
+
+from user_defined_queue.repository.user_defined_queue_repository_impl import UserDefinedQueueRepositoryImpl
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template', 'include', 'socket_server'))
+
+from template.include.socket_server.utility.color_print import ColorPrinter
+
 from openai_api_test.repository.openai_api_test_repository_impl import OpenaiApiTestRepositoryImpl
 from openai_api_test.service.openai_api_test_service import OpenaiApiTestService
 
 
 class OpenaiApiTestServiceImpl(OpenaiApiTestService):
-    def __init__(self):
+    def __init__(self, userDefinedQueueRepository: UserDefinedQueueRepositoryImpl):
         self.__openaiApiTestRepository = OpenaiApiTestRepositoryImpl()
+        self.__userDefinedQueueRepository = userDefinedQueueRepository
 
-    async def letsChat(self, userSendMessage):
-        return await self.__openaiApiTestRepository.generateText(userSendMessage)
+    async def letsChat(self):
+        userDefinedReceiverFastAPIChannel = self.__userDefinedQueueRepository.getUserDefinedSocketReceiverFastAPIChannel()
+        ColorPrinter.print_important_data("userDefinedReceiverFastAPIChannel", userDefinedReceiverFastAPIChannel)
+        return await self.__openaiApiTestRepository.generateText(userDefinedReceiverFastAPIChannel)
